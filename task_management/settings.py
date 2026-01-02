1a75573a1f686c9544b6cb20650595ca84d69759
farid <faridali9818@gmail.com>
2026-01-02 17:05:58 +0600

    Prepare for repo rename

--- task_management/settings.py
+++ task_management/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 # ALLOWED_HOSTS = [] 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com']  
 
 # =======================
 # Installed Apps
